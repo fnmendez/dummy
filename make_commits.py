ed71bd5285cfee569218ab8569d5fb3a62f963e4
Franco Mendez Z <f@francomendez.com>
2022-03-31 09:51:27 -0300

    add config

--- make_commits.py
+++ make_commits.py
@@ -27,6 +27,9 @@ messages=[
 # I couldn't get the bash for loop to run in the github action, so... here we are.
 for n in range(random.randint(1, 10)):
     to_write = f'{random.choice(messages)}\n' * 100
-    os.system(f'echo "{to_write}" >> dump/dump-{random.randint(1, 10)}.txt')
+    if n < 3:
+        os.system(f'echo "{to_write}" >> dump/dump-{random.randint(1, 10)}.txt')
+    else:
+        os.system(f'echo "{to_write}" > dump/dump-{random.randint(1, 10)}.txt')
     os.system('git add .')
     os.system(f'git commit -m "{q(random.choice(messages))}"')
